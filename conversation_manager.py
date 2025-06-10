17de586a6dd797fef5262c9e245a49e5b42eb71f
Yehonatan Bar <43821476+Yehonatan-Bar@users.noreply.github.com>
2025-06-10 13:46:16 +0300

    Update conversation_manager.py

--- conversation_manager.py
+++ conversation_manager.py
@@ -490,4 +490,4 @@ def index():
                            result=None)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=True)
